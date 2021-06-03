6fb6fa53d443a886436ddbef2d68c7cf940594d5
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2021-06-03 20:26:28 -0300

    Adicionando gráfico nos resultados de uma avaliação

--- bq_new_front/src/pages/Student/EvaluationsResult/EvaluationResultDetails/EvaluationsResultDetails.js
+++ bq_new_front/src/pages/Student/EvaluationsResult/EvaluationResultDetails/EvaluationsResultDetails.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import Chart from "react-google-charts";
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
@@ -9,12 +10,14 @@ import {
   Divider,
   IconButton,
   Typography, Grid, Tooltip,
-  Paper, LinearProgress
+  Paper, LinearProgress, Box
 } from '@material-ui/core';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import api from "../../../../services/api";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import {Close, Done, Block} from "@material-ui/icons";
-import {withStyles} from "@material-ui/core/styles";
+import { Close, Done, Block } from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
 import Swal from "sweetalert2";
 
 const useStyles = makeStyles(() => ({
@@ -118,11 +121,30 @@ const TooltipCustomized = withStyles((theme) => ({
   },
 }))(Tooltip);
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+      <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`nav-tabpanel-${index}`}
+          aria-labelledby={`nav-tab-${index}`}
+          {...other}>
+          {value === index && (
+              <Box p={3}>
+                  <Typography>{children}</Typography>
+              </Box>
+          )}
+      </div>
+  );
+}
+
 const EvaluationsResultDetails = props => {
   const { className, history, ...rest } = props;
   const { idHead } = props.match.params;
-  const [ head, setHead ] = useState([]);
-  const [ questions, setQuestions ] = useState(null);
+  const [head, setHead] = useState([]);
+  const [questions, setQuestions] = useState(null);
 
   const classes = useStyles();
 
@@ -146,16 +168,16 @@ const EvaluationsResultDetails = props => {
     });
   }
 
-  async function findHead(){
+  async function findHead() {
     try {
 
-      const response = await api.get('/evaluation/student/result/evaluations-specific/'+idHead);
+      const response = await api.get('/evaluation/student/result/evaluations-specific/' + idHead);
 
       if (response.status == 200) {
         setQuestions(response.data.questions);
         setHead(response.data);
-      } if (response.status == 202 ){
-        if(response.data.message) {
+      } if (response.status == 202) {
+        if (response.data.message) {
           loadAlert('error', response.data.message);
           setQuestions([]);
           setHead([]);
@@ -178,104 +200,210 @@ const EvaluationsResultDetails = props => {
     history.goBack();
   };
 
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleChangeTab = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `nav-tab-${index}`,
+      'aria-controls': `nav-tabpanel-${index}`,
+    };
+  }
+
+  function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+
   return (
-      <div>
-        <Card
-            {...rest}
-            className={clsx(classes.root, className)}>
-          <div className={classes.contentHeader}>
-            <IconButton onClick={handleBack}>
-              <ArrowBackIcon />
-            </IconButton>
-          </div>
-          <CardHeader
-              subheader=""
-              title="Avaliação"/>
-          <Divider />
-            <CardContent>
-              { questions == null ?
-                  <LinearProgress color="secondary"    />
-                  :
-                  <div>
-                    {head.qtdCorrect != null ?
-                    <Paper variant="outlined" style={{padding: '5px', marginBottom: '15px'}}>
-                      <Typography align="center"
-                                  variant="body2" color="textPrimary"
-                                  style={{fontWeight: 'bold', fontSize: '14px', marginRight: '5px',
-                                    color: '#009688'}} >
-                        {head.qtdCorrect >= 2 ? 'Você acertou '+head.qtdCorrect+ ' questões.'
-                            : 'Você acertou '+head.qtdCorrect+ ' questão.'}
-                      </Typography>
-                      <Typography align="center"
-                                  variant="body2" color="textPrimary"
-                                  style={{fontWeight: 'bold', fontSize: '14px', marginRight: '5px',
-                                    color: '#EC0B43'}} >
-                        {head.qtdIncorrect >= 2 ? 'Você errou '+head.qtdIncorrect+ ' questões.'
-                            : 'Você errou '+head.qtdIncorrect+ ' questão.'}
-                      </Typography>
+    <div>
+      <Card
+        {...rest}
+        className={clsx(classes.root, className)}>
+        <div className={classes.contentHeader}>
+          <IconButton onClick={handleBack}>
+            <ArrowBackIcon />
+          </IconButton>
+        </div>
+        <CardHeader
+          subheader=""
+          title="Avaliação" />
+        <Divider />
+        <CardContent>
+          {questions == null ?
+            <LinearProgress color="secondary" />
+            :
+            <div>
+              {head.qtdCorrect != null ?
+                <Paper variant="outlined" style={{ padding: '5px', marginBottom: '15px' }}>
+                  <Typography align="center"
+                    variant="body2" color="textPrimary"
+                    style={{
+                      fontWeight: 'bold', fontSize: '14px', marginRight: '5px',
+                      color: '#009688'
+                    }} >
+                    {head.qtdCorrect >= 2 ? 'Você acertou ' + head.qtdCorrect + ' questões.'
+                      : 'Você acertou ' + head.qtdCorrect + ' questão.'}
+                  </Typography>
+                  <Typography align="center"
+                    variant="body2" color="textPrimary"
+                    style={{
+                      fontWeight: 'bold', fontSize: '14px', marginRight: '5px',
+                      color: '#EC0B43'
+                    }} >
+                    {head.qtdIncorrect >= 2 ? 'Você errou ' + head.qtdIncorrect + ' questões.'
+                      : 'Você errou ' + head.qtdIncorrect + ' questão.'}
+                  </Typography>
 
-                    </Paper>
-                    : null}
+                </Paper>
+                : null}
 
-                    {questions.map((result, i) => (
-                        <div style={{marginBottom: '30px'}}>
-                          <Typography align="center" style={{fontWeight: 'bold'}}
-                                      variant="h5" component="h2">
-                            {'Questão ' + (i+1)}
-                          </Typography>
-                          {result.skill ?
-                              <Grid
-                                  container
-                                  direction="row"
-                                  justify="center"
-                                  alignItems="center">
-                                <Typography align="center"
-                                            variant="body2" color="textPrimary"
-                                            style={{fontWeight: 'bold', marginRight: '5px'}} >
-                                  Competência:
-                                </Typography>
-                                <Typography align="center"
-                                            variant="body2" color="textPrimary" >
-                                  {result.skill.description}
-                                </Typography>
-                              </Grid>
-                              : null }
-                          {result.objects ?
-                              <Grid
-                                  container
-                                  direction="row"
-                                  justify="center"
-                                  alignItems="center">
-                                  <Typography align="center"
-                                              variant="body2" color="textPrimary"
-                                              style={{fontWeight: 'bold', marginRight: '5px'}} >
-                                    Objeto(s) de Conhecimento:
+              <Tabs
+                variant="fullWidth"
+                value={tabValue}
+                onChange={handleChangeTab}
+                aria-label="nav tabs example"
+              >
+                <LinkTab label="Questões" href="#" {...a11yProps(0)} />
+                <LinkTab label="Gráfico" href="#" {...a11yProps(1)} />
+              </Tabs>
+
+              <TabPanel value={tabValue} index={0}>
+                {questions.map((result, i) => (
+                  <div style={{ marginBottom: '30px' }}>
+                    <Typography align="center" style={{ fontWeight: 'bold' }}
+                      variant="h5" component="h2">
+                      {'Questão ' + (i + 1)}
+                    </Typography>
+                    {result.skill ?
+                      <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center">
+                        <Typography align="center"
+                          variant="body2" color="textPrimary"
+                          style={{ fontWeight: 'bold', marginRight: '5px' }} >
+                          Competência:
                                   </Typography>
-                                  <Typography align="center"
-                                              variant="body2" color="textPrimary" >
-                                      {result.objects.map(item => (
-                                            item.object.description + '; '
-                                      ))}
-                                  </Typography>
-                              </Grid>
-                              : null }
-                            {result.answer == null ?
-                                    <span className={classes.percentageNull}><Block /></span>
-                                    :
-                                    result.correct == 1 ?
-                                        <span className={classes.percentageGreen}><Done /></span>
-                                         :
-                                        <span className={classes.percentageRed}><Close /></span>}
-                        </div>
-
-                    ))}
+                        <Typography align="center"
+                          variant="body2" color="textPrimary" >
+                          {result.skill.description}
+                        </Typography>
+                      </Grid>
+                      : null}
+                    {result.objects ?
+                      <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center">
+                        <Typography align="center"
+                          variant="body2" color="textPrimary"
+                          style={{ fontWeight: 'bold', marginRight: '5px' }} >
+                          Objeto(s) de Conhecimento:
+                                    </Typography>
+                        <Typography align="center"
+                          variant="body2" color="textPrimary" >
+                          {result.objects.map(item => (
+                            item.object.description + '; '
+                          ))}
+                        </Typography>
+                      </Grid>
+                      : null}
+                    {result.answer == null ?
+                      <span className={classes.percentageNull}><Block /></span>
+                      :
+                      result.correct == 1 ?
+                        <span className={classes.percentageGreen}><Done /></span>
+                        :
+                        <span className={classes.percentageRed}><Close /></span>}
                   </div>
+                ))}
 
-              }
+                <Chart
+                  width={1366}
+                  height={720}
+                  chartType="ColumnChart"
+                  loader={<div>Caregando gráfico</div>}
+                  data={
+                    questions.map((result, i) => ([`Questão ${i + 1}`, result.correct]))
+                  }
+                  options={{
+                    title: 'Resultado das respostas',
+                    chartArea: { width: '30%' },
+                    hAxis: {
+                      title: 'Questão',
+                      minValue: 0,
+                    },
+                    vAxis: {
+                      title: 'Acertadas/Erradas',
+                    },
+                  }}
+                  legendToggle
+                />
+              </TabPanel>
 
-            </CardContent>
-       </Card>
-      </div>
+              {console.log(questions.map((result, i) => ([`Questão ${i + 1}`, result.correct == 1 ? 0 : 1])))}
+
+              {/* [
+                    ['City', '2010 Population', '2000 Population'],
+                    ['New York City, NY', 8175000, 8008000],
+                    ['Los Angeles, CA', 3792000, 3694000],
+                    ['Chicago, IL', 2695000, 2896000],
+                    ['Houston, TX', 2099000, 1953000],
+                    ['Philadelphia, PA', 1526000, 1517000],
+                  ] */}
+
+              <TabPanel value={tabValue} index={1}>
+                <Chart
+                  width="100%"
+                  height={200}
+                  chartType="ColumnChart"
+                  loader={<div>Caregando gráfico</div>}
+                  data={
+                    [
+                      ['Acerto', 'Resposta correta'],
+                    ].concat(questions.map((result, i) => {
+                      return [`Questão ${i + 1}`, result.correct && result.correct == 1 ? 1 : 0];
+                    }))
+                  }
+                  options={{
+                    title: 'Resultado das respostas',
+                    chartArea: { width: '80%' },
+                    hAxis: {
+                      title: 'Questão',
+                      minValue: 0,
+                      viewWindow: {
+                        min: 0
+                      }
+                    },
+                    vAxis: {
+                      title: 'Acertadas/Erradas',
+                      // viewWindowMode:'explicit',
+                      viewWindow: {
+                        min: 0
+                      }
+                    },
+                  }}
+                  legendToggle
+                />
+              </TabPanel>
+            </div>
+          }
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
