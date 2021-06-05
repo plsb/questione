8207ebe22cbe92858c000bbc7914418942037313
plsb <peluissaraiva@gmail.com>
2021-06-04 22:24:22 -0300

    - Ajustes após teste.

--- bq_new_front/src/components/index.js
+++ bq_new_front/src/components/index.js
@@ -2,7 +2,6 @@ export { default as StatusBullet } from './StatusBullet';
 export { default as RouteWithLayout } from './RouteWithLayout';
 export { default as SearchInput } from './SearchInput';
 export { default as DialogQuestione } from './DialogQuestione';
-export { default as SnackbarQuestione } from './Snackbar';
 export { default as StartEvaluationCard } from './StartEvaluationCard';
 export { default as TableQuestione } from './TableQuestione';
 export { default as QuestionCard } from './QuestionCard';
