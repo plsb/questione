520cade8dc5f66b43d5a2eeddd3619987de884d4
plsb <peluissaraiva@gmail.com>
2021-07-05 19:25:25 -0300

    - adicão da descricão da avaliacão na rota evaluation/student/result/evaluations-specific/

--- bq_api/app/Http/Controllers/ResultEvaluationStudent.php
+++ bq_api/app/Http/Controllers/ResultEvaluationStudent.php
@@ -150,6 +150,7 @@ class ResultEvaluationStudent extends Controller
             'id' => $head_answer->id,
             'qtdCorrect' => $qtdCorrect,
             'qtdIncorrect' => $qtdIncorrect,
+            'evaluation' => $application_evaluation->evaluation,
             'questions' => $resultAnswer,
             'question_preview' => $preview_question
 
