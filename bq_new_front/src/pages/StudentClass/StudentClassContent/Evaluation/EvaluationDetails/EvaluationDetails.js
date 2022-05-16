e40fa1cc2c9712d8b092a7261cdba10958d1beb9
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2022-05-16 19:59:18 -0300

    Adicionando listagem de avaliações da turma

---
+++ bq_new_front/src/pages/StudentClass/StudentClassContent/Evaluation/EvaluationDetails/EvaluationDetails.js
@@ -0,0 +1,254 @@
+import React, {useEffect, useState} from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  Grid,
+  Button,
+  TextField, IconButton,
+  TableBody, Table, TableCell,
+  TableRow, TableHead, TablePagination, LinearProgress
+} from '@material-ui/core';
+import api from "../../../../services/api";
+import { toast } from 'react-toastify';
+import validate from "validate.js";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import QuestionCard from "../../../../components/QuestionCard";
+
+const schema = {
+  description: {
+    presence: {allowEmpty: false, message: 'A descrição é obrigatória.'},
+    length: {
+      minimum: 4,
+      maximum: 300,
+      message: 'A descrição deve conter no mínimo 4 e no máximo 300 caracteres.'
+    }
+  }
+};
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  headTable: {
+    fontWeight: "bold"
+  },
+  fab:{
+    backgroundColor: '#009688',
+    color: '#e0f2f1',
+  },
+  labelRed: {
+    backgroundColor: '#EC0B43',
+    display: 'block',
+    margin: '10px',
+    padding: '5px',
+    textAlign: 'center',
+    color: '#fff',
+    borderRadius: 4
+  },
+}));
+
+const EvaluationDetails = props => {
+  const { className, history, ...rest } = props;
+  const { codigoEvaluation } = props.match.params;
+
+  const classes = useStyles();
+
+  const [questions, setQuestions] = useState(null);
+  const [refresh, setRefresh] = React.useState(0);
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {}
+  });
+
+  async function saveEvaluationDetails(){
+    try {
+      const description = formState.values.description;
+
+      const id = formState.values.id;
+      const data = {
+        description
+      }
+      let response= {};
+      let acao = "";
+      if(!id) {
+         response = await api.post('evaluation', data);
+         acao = "cadastrada";
+      } else {
+         response = await api.put('evaluation/'+id, data);
+        acao = "atualizada";
+      }
+      if (response.status === 202) {
+        if(response.data.message){
+          toast.error(response.data.message);
+        } else if(response.data.errors[0].description){
+          toast.error(response.data.errors[0].description);
+        }
+      } else {
+        toast.success('Avaluação '+acao+'.');
+        history.push('/evaluations');
+      }
+
+    } catch (error) {
+
+    }
+  }
+
+  async function loadQuestionsEvaluation(id, page){
+    try {
+      const response = await api.get('evaluation/show/questions/'+id+'?page='+page);
+      if (response.status === 200) {
+        setQuestions(response.data.data);
+        setTotal(response.data.total);
+      } else {
+        setQuestions([]);
+      }
+    } catch (error) {
+
+    }
+  }
+
+  async function findAEvaluation(id){
+    try {
+      const response = await api.get('evaluation/show/'+id);
+      if (response.status === 202) {
+        if(response.data.message){
+          toast.error(response.data.message);
+        }
+      } else {
+        setFormState(formState => ({
+          values: {
+            'questions': response.data[0].questions,
+            'description': response.data[0].description,
+            'id': response.data[0].id
+          },
+          touched: {
+            ...formState.touched,
+          }
+        }));
+        //setQuestions(response.data[0].questions);
+      }
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    if(codigoEvaluation){
+      findAEvaluation(codigoEvaluation);
+      loadQuestionsEvaluation(codigoEvaluation);
+    } else {
+      setQuestions([]);
+    }
+
+  }, [refresh]);
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: (errors || formState.values.course==0) ? false : true,
+      errors: errors || {}
+    }));
+  }, [formState.values, questions]);
+
+  const handleChange = event => {
+    setFormState({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]: event.target.value
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true
+      }
+    });
+  };
+
+  const hasError = field =>
+      formState.touched[field] && formState.errors[field] ? true : false;
+
+  const handleBack = () => {
+    history.goBack();
+  };
+
+  const handlePageChange = (event, page) => {
+    if(codigoEvaluation) {
+      loadQuestionsEvaluation(codigoEvaluation, page + 1)
+    }
+    setPage(page);
+  };
+
+  const handleRowsPerPageChange = event => {
+    setRowsPerPage(event.target.value);
+  };
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}>
+      <form
+        autoComplete="off">
+        <div className={classes.contentHeader}>
+          <IconButton onClick={handleBack}>
+            <ArrowBackIcon />
+          </IconButton>
+        </div>
+        <CardHeader
+          subheader=""
+          title="Avaliação"/>
+        <Divider />
+        <CardContent>
+          <Grid
+            container
+            spacing={1}>
+            <Grid
+              item
+              md={12}
+              xs={12}>
+              <TextField
+                fullWidth
+                error={hasError('description')}
+                helperText={
+                  hasError('description') ? formState.errors.description[0] : null
+                }
+                label="Descrição"
+                margin="dense"
+                name="description"
+                onChange={handleChange}
+                value={formState.values.description || ''}
+                variant="outlined"
+              />
+              <Button
+                  color="primary"
+                  variant="outlined"
+                  disabled={!formState.isValid}
+                  onClick={saveEvaluationDetails}>
+                Salvar
+              </Button>
+            </Grid>
+            <Divider />
+          </Grid>
+        </CardContent>
+        <Divider />
+      </form>
+    </Card>
+  );
+};
+
+EvaluationDetails.propTypes = {
+  className: PropTypes.string,
+};
+
+export default EvaluationDetails;
---
+++ bq_new_front/src/pages/StudentClass/StudentClassContent/Evaluation/EvaluationQuestions/components/EvaluationQuestionCard.js
@@ -0,0 +1,309 @@
+import React, { useEffect } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import {
+    IconButton,
+    MenuItem, Menu,
+    Typography,
+    Tooltip,
+    Paper, Box, Grid, Button
+} from '@material-ui/core';
+import { MoreVert, Delete as DeleteIcon } from '@material-ui/icons';
+import { withRouter } from "react-router-dom";
+import ReactHtmlParser from 'react-html-parser';
+import api from '../../../../../services/api';
+import DialogQuestione from '../../../../../components/DialogQuestione';
+import { toast } from 'react-toastify';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        marginTop: 5,
+        marginBottom: 10,
+        marginRigth: 0,
+        marginLeft: 5,
+        width: '100%'
+    },
+    head: {
+        paddingBottom: 0,
+        paddingTop: 10
+    },
+    chipGreen: {
+        backgroundColor: '#4db6ac',
+        color: '#ffffff',
+        marginRight: 2
+    },
+    chipRed: {
+        backgroundColor: '#f44336',
+        color: '#ffffff',
+        marginRight: 2
+    },
+    spacer: {
+        flexGrow: 1
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    paper: {
+        display: 'flex',
+        marginBottom: 10,
+        '& > *': {
+            margin: theme.spacing(2),
+        },
+    },
+    paperWrong: {
+        backgroundColor: '#ef9a9a',
+        color: '#212121',
+        margin: 3,
+        padding: 8
+    },
+    paperRight: {
+        backgroundColor: '#80cbc4',
+        color: '#212121',
+        margin: 3,
+        padding: 8
+    },
+    appBar: {
+        position: 'relative',
+    },
+    title: {
+        marginLeft: theme.spacing(2),
+        flex: 1,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    labelRank: {
+        textAlign: 'right'
+    },
+    lineQuestion: {
+        marginLeft: 20,
+    },
+    content: {
+        width: '100%',
+    },
+    questionActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    deleteButton: {
+        padding: '0px',
+        width: '32px',
+        minWidth: '32px',
+        height: '32px',
+        color: '#ff6333'
+    },
+    correct: {
+        backgroundColor: '#80cbc4',
+    },
+}));
+
+const QuestionCard = props => {
+    const {
+        className,
+        history,
+        question,
+        setRefresh,
+        refresh,
+        id_evaluation,
+        data,
+        setQuestions,
+        hasApplication,
+        ...rest
+     } = props;
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [openDeleteQuestionEvaluation, setOpenDeleteQuestionEvaluation] = React.useState(false);
+    const [openDeleteQuestion, setOpenDeleteQuestion] = React.useState(false);
+
+    const classes = useStyles();
+
+    /* function loadRank(){
+
+         if(question.rank_avg.length !== 0){
+             setRank(question.rank_avg[0].rank_avg);
+         }
+         if(question.rank_by_user_active.length == 0 &&
+             question.fk_user_id !== localStorage.getItem("@Questione-id-user")) {
+             setRank(0)
+         }
+         setQtRank(question.rank_count);
+     }*/
+
+    // useEffect(() => {
+    //     loadRank();
+    // }, [question, rank]);
+
+    useEffect(() => {
+
+    }, [openDeleteQuestionEvaluation]);
+
+    // useEffect(() => {
+    //     loadRank();
+    //     loadEvaluations();
+    // }, []);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const onClickOpenDialogQEvaluation = () => {
+        setOpenDeleteQuestionEvaluation(true);
+    }
+
+    const onClickCloseDialogQEvaluation = () => {
+        setOpenDeleteQuestionEvaluation(false);
+    }
+
+    const onClickOpenDialogQuestion = () => {
+        setOpenDeleteQuestion(true);
+    }
+
+    // async function deleteQuestion() {
+    //     setOpenDeleteQuestion(false);
+    //     try {
+    //         let url = 'question/' + question.id;
+
+    //         const response = await api.delete(url);
+    //         if (response.status === 202) {
+    //             if (response.data.message) {
+    //                 toast.error(response.data.message);
+    //             }
+    //         } else {
+    //             toast.success('Questão excluída.');
+    //             setRefresh(refresh + 1);
+    //         }
+    //     } catch (error) {
+
+    //     }
+    // }
+
+    async function deleteQuestionEvaluation() {
+        setOpenDeleteQuestionEvaluation(false);
+
+        try {
+            let url = 'evaluation/deletequestion/' + question.fk_question_id + '+?fk_evaluation_id=' + id_evaluation;
+            const fk_evaluation_id = id_evaluation;
+            const data = {
+                fk_evaluation_id
+            }
+            const response = await api.delete(url);
+            if (response.status === 202) {
+                if (response.data.message) {
+                    toast.error(response.data.message);
+                }
+            } else {
+                toast.success('Questão excluída da avaliação.');
+                setQuestions((lastQuestions) => lastQuestions.filter((currentQuestion) => currentQuestion.id !== question.id));
+                setRefresh(refresh + 1);
+            }
+        } catch (error) {
+
+        }
+    }
+
+    return (
+        <div className={classes.content}>
+            <div className={classes.lineQuestion}>
+                <div className={classes.questionActions}>
+                    {hasApplication == '0' && (
+                        <Tooltip title="Excluir questão da avaliação">
+                            <Button onClick={onClickOpenDialogQEvaluation} className={classes.deleteButton}>
+                                <DeleteIcon />
+                            </Button>
+                        </Tooltip>
+                    )}
+
+                    {/* <Box display="flex" justifyContent="flex-end" p={1} m={1} bgcolor="background.paper">
+                        <Tooltip title="Opções">
+                            <Box flexDirection="row" alignSelf="flex-end">
+                                <IconButton className={classes.labelRank} aria-label="settings"
+                                    onClick={handleClick}>
+                                    <MoreVert />
+                                </IconButton>
+                            </Box>
+                        </Tooltip>
+                    </Box>
+
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={onClickOpenDialogQEvaluation}>Excluir da Avaliação</MenuItem>
+                    </Menu> */}
+                    <DialogQuestione
+                        handleClose={onClickCloseDialogQEvaluation}
+                        open={openDeleteQuestionEvaluation}
+                        onClickAgree={deleteQuestionEvaluation}
+                        onClickDisagree={onClickCloseDialogQEvaluation}
+                        mesage={'Deseja excluir a questão selecionada da avaliação?'}
+                        title={'Excluir Questão da Avaliação'}
+                    />
+                </div>
+
+                {question.question.skill ?
+                    <div>
+                        <Typography variant="button" color="textSecondary" component="p">
+                            Competência:
+                        </Typography>
+                        {ReactHtmlParser(question.question.skill.description)}
+                    </div>
+                    : null}
+                {question.question.knowledge_objects ?
+                    <div>
+                        <Typography variant="button" color="textSecondary" component="p">
+                            Objeto(s) de Conhecimento:
+                        </Typography>
+                        {question.question.knowledge_objects.map(item => (
+                            ReactHtmlParser(item.description) + '; '
+                        ))}
+                    </div>
+                    : null}
+                    <br />
+                <Typography variant="button" color="textSecondary" component="p">
+                    Texto base:
+                </Typography>
+                <div> {ReactHtmlParser(question.question.base_text)} </div>
+                <br />
+                <Typography variant="button" color="textSecondary" component="p">
+                    Enunciado:
+                </Typography>
+                <div> {ReactHtmlParser(question.question.stem)} </div>
+                <br />
+                <Typography variant="button" color="textSecondary" component="p">
+                    Alternativas:
+                </Typography>
+                <br />
+                {question.question.question_items.map(item => (
+                    <div>
+                        {console.log(item)}
+                        <Paper className={clsx(classes.paper, item.correct_item == 1 ?  classes.correct : null)} variant="outlined"> {ReactHtmlParser(item.description)} </Paper>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+QuestionCard.propTypes = {
+    className: PropTypes.string,
+    question: PropTypes.object,
+    id_evaluation: PropTypes.object,
+    evaluations: PropTypes.object,
+    history: PropTypes.object,
+    setRefresh: PropTypes.func,
+    refresh: PropTypes.number,
+
+};
+
+export default withRouter(QuestionCard);
