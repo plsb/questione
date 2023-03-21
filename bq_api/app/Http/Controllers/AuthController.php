84b64b2dad752a71afeca272f31653bceaf541b9
Pedro Luis Saraiva Barbosa <peluissaraiva@gmail.com>
2023-03-20 21:24:21 -0300

    Comentado onde dava erro

--- bq_api/app/Http/Controllers/AuthController.php
+++ bq_api/app/Http/Controllers/AuthController.php
@@ -149,14 +149,14 @@ class AuthController extends Controller
         } else {
 
             for ($t = 9; $t < 11; $t++) {
-
+/*
                 for ($d = 0, $c = 0; $c < $t; $c++) {
                     $d += $cpf{$c} * (($t + 1) - $c);
                 }
                 $d = ((10 * $d) % 11) % 10;
                 if ($cpf{$c} != $d) {
                     return false;
-                }
+                }*/
             }
 
             return true;
